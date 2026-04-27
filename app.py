85e3f8a553e916cf3ca588792ebf9bd8aa11d5f0
Abner <abnereduardo.1009@gmail.com>
2026-04-27 19:55:01 -0300

    salvando antes do pull

--- app.py
+++ app.py
@@ -49,4 +49,43 @@ def carregar_dados():
 def salvar_dados(dados):
     with open(ARQUIVO, "w", ecoding="utf-8") as f:
         json.dump(dados, f, indent=4, ensure_ascii=False)
+
+def buscar_tarefa_por_id(tarefa_id):
+    dados = carregar_dados()
+    for tarefa in dados["tarefas"]:
+        if tarefa["id"] == tarefa_id:
+            return tarefa, dados
+        return None, dados 
+
+#------------------
+#ROTAS
+#------------------
+@app.get("/")
+def home():
+    return {"mensagem": "LifeOS API - rodando com sucesso!"}
+
+@app.post("/tarefas", response_model=Tarefa)
+def criar_tarefa(tarefa: TarefaCriar):
+    dados = carregar_dados()
+
+    nova_tarefa = {
+        "id": str(uuid4()),
+        "titulo": tarefa.titulo,
+        "descricacao" :  tarefa.descricacao,
+        "criado_em" : datetime.now().strtime("%d/%m/%Y %H:%M:%S"),
+        "concluida" : False
+
+    }
+
+    dados["tarefas"].append(nova_tarefa)
+    salvar_dados(dados)
+
+    return nova_tarefa
+
+@app.get("/tarefas", response_model=List[Tarefa])
+def Listar_tarefas():
+    dados = carregar_daddos()
+    return dados["tarefas"]
+
+@app.get("")
         
